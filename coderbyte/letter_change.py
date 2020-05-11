4c37f4596ce0202d8a63ff9ffb5916e9c95f0185
ved93 <vpved93@gmail.com>
2020-05-11 21:28:34 +0530

    "added new files"

---
+++ coderbyte/letter_change.py
@@ -0,0 +1,23 @@
+def LetterChanges(a_string):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    encoded_string = ""
+    for letter in a_string.lower():
+        if letter not in letters:
+            encoded_string += letter
+        elif letter == "z":
+            encoded_string += "a"
+        else:
+            index = letters.index(letter)
+            encoded_string += letters[index + 1]
+    capitalized_encoded = ""
+
+    for char in encoded_string:
+        if char in "aeiou":
+            capitalized_encoded += char.upper()
+        else:
+            capitalized_encoded += char
+    return capitalized_encoded
+
+
+# keep this function call here
+print(LetterChanges(input()))
