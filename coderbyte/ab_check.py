c2129f82b5f00605bf1eb8c925bc0bfa224a7dab
ved93 <vpved93@gmail.com>
2020-05-12 12:14:29 +0530

    ""

---
+++ coderbyte/ab_check.py
@@ -0,0 +1,15 @@
+#  Using the Python language, have the function ABCheck(str) take the str parameter being passed and return the string
+# true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie.
+# "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise
+# return the string false.
+
+
+def ABCheck(str):
+    for index in range(len(str) - 4):
+        if str[index] == "a":
+            if str[index + 4] == "b":
+                return True
+        elif str[index] == "b":
+            if str[index + 4] == "a":
+                return True
+    return False
