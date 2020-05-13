d2ccd3778514d9d290343d3d9332ea682cb3896f
ved93 <vpved93@gmail.com>
2020-05-13 22:12:31 +0530

    ""

--- coderbyte/medium/number_encoding.py
+++ coderbyte/medium/number_encoding.py
@@ -1,7 +1,7 @@
 # Using the Python language, have the function NumberEncoding(str) take the str parameter and encode the message
 # according to the following rule: encode every letter into its corresponding numbered position in the alphabet.
 # Symbols and spaces will also be used in the input. For example: if str is "af5c a#!" then your program should
-# return 1653 1#!.
+# return 1653 1#!
 
 
 def NumberEncoding(a_string):
