9f8aa446e41242f74fdd25e4a79e37a44a5b3048
ved93 <vpved93@gmail.com>
2020-05-26 22:38:51 +0530

    leetcode day 1

---
+++ leetcode/two_sum.py
@@ -0,0 +1,33 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         i = 0
+#         n = len(nums)
+#         while i <= n:
+#             for j in range(i,n):
+#                 if nums[i]+nums[j] == target :
+#                     return [i,j]
+#             i = i+1
+
+# def two_sum(nums, target):
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        r = []
+
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            if temp in r:
+                return [i, nums.index(temp)]
+
+            r.append(nums[i])
+
+
+# nums =   list(map(int,input().split()))
+# target =  int(input())
+
+if __name__ == "__main__":
+    solution = Solution()
+    # solution.twoSum(nums, target)
+    print(solution.twoSum([2, 7, 11, 15], 9))
+
