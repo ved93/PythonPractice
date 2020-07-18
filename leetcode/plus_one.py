668f0e76fa76c02bff4d3e348a4d5e55af0e47ac
ved93 <vpved93@gmail.com>
2020-07-18 09:48:09 +0530

    ""

---
+++ leetcode/plus_one.py
@@ -0,0 +1,37 @@
+
+# Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+# The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+# You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+# Example 1:
+
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+
+
+
+
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        
+        # move along the input array starting from the end
+        for i in range(n):
+            idx = n - 1 - i
+            # set all the nines at the end of array to zeros
+            if digits[idx] == 9:
+                digits[idx] = 0
+            # here we have the rightmost not-nine
+            else:
+                # increase this rightmost not-nine by 1 
+                digits[idx] += 1
+                # and the job is done
+                return digits
+                
+        # we're here because all the digits are nines
+        return [1] + digits
