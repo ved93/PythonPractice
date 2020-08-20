d1b7eb96bb2ca5947de0f9d3cfac0d85a7073a76
ved93 <vpved93@gmail.com>
2020-08-20 06:49:17 +0530

    added new files

--- leetcode/climb_stairs.py
+++ leetcode/climb_stairs.py
@@ -1,5 +1,5 @@
 
-
+# https://leetcode.com/problems/climbing-stairs/
 
 
 
@@ -19,10 +19,13 @@ class Solution:
 def climbStairs2(self, n):
     if n == 1:
         return 1
-    res = [0 for i in range(n)]
-    res[0], res[1] = 1, 2
-    for i in range(2, n):
-        res[i] = res[i-1] + res[i-2]
+    res = [-1 for i in range(n+1)]
+    res[0] = 1
+    res[1] = 1
+    
+    for i in range(2,n+1):
+        res[i] = res[i-1]+res[i-2]
+        
     return res[-1]
 
 
@@ -41,7 +44,7 @@ def climbStairs3(self, n):
 def climbStairs4(self, n):
     if n == 1:
         return 1
-    dic = [-1 for i in xrange(n)]
+    dic = [-1 for i in range(n)]
     dic[0], dic[1] = 1, 2
     return self.helper(n-1, dic)
     
@@ -58,3 +61,37 @@ def climbStairs(self, n):
     if n not in self.dic:
         self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
     return self.dic[n]
+
+
+
+
+
+
+
+class Solution:
+    def climbStairs(self, n: int):
+        ## RC ##
+        ## APPROACH 1 : RECURSION ##
+        # must do approach to understand concept.
+        # Recursion has :
+        # 1. base condition (to exit loop) i.e i>n => return 0  or i==n return 1
+        # 2. recursive call : climb(i+1,n) + climb(i+2,n)
+        
+        ## APPROACH 2 : DP ##
+        #   1. top can be reached from (N-1)th Step or (N-2)th Step i.e ===> dp[N-1] + dp[N-2]
+        #   2. base case :                                              No of ways
+        #   0 steps     ===>    0 step                                      0
+        #   1 steps     ===>    1 step                                      1   
+        #   2 steps     ===>    (1 + 1 steps) or (2 steps)                  2       
+        
+        #   FINDING DP PATTERN
+        #   3 steps     ===>    (1+1+1) (1+2) (2+1) (3)                     3
+        #   4 steps     ===>    (1+1+1+1) (1+2+1) (2+1+1) (1+1+2) (2+2)     5   (pattern found n-1 + n-2 )
+        
+        if(n==1): return 1
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]                           # last position will have solution.
---
+++ leetcode/median_two_sorted_array.py
@@ -0,0 +1,6 @@
+
+#https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/277759/Python-O(Log(min(mn))-solution-Simple-code
+
+# https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2511/Intuitive-Python-O(log-(m%2Bn))-solution-by-kth-smallest-in-the-two-sorted-arrays-252ms
+
+# https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2755/9-lines-O(log(min(mn)))-Python
