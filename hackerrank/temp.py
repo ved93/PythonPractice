38260757f4e70078dac4f4c93eb2e04e5443a633
ved93 <vpved93@gmail.com>
2020-10-16 08:06:17 +0530

    updated

---
+++ hackerrank/temp.py
@@ -0,0 +1,2 @@
+# https://github.com/charlesartbr/hackerrank-python/tree/master/10-days-of-statistics
+
---
+++ mooc/main.py
@@ -0,0 +1,5 @@
+
+import sys
+n = int(sys.argv[1])
+print(n)
+print(' '.join([str(i*2) for i in range(n)]))
